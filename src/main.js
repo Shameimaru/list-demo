e052cb41174179c9dcba499d3b0e9a1974c570c4
Administrator <1>
2017-04-21 11:31:37 +0800

    A.Add focus directive

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store/store'
+import directives from './directives';
 
-console.log(directives);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
